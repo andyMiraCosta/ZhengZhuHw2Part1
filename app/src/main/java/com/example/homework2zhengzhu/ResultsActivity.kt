1301dfe7824d191a239fec908daebfddaf6d2d86
Andy <zhumeilun0103@gmail.com>
2024-09-30 22:39:44 -0700

    initial commit

--- app/src/main/java/com/example/homework2zhengzhu/ResultsActivity.kt
+++ app/src/main/java/com/example/homework2zhengzhu/ResultsActivity.kt
@@ -1,32 +1,20 @@
 package com.example.homework2zhengzhu
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_results)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button: Button = findViewById<Button>(R.id.btnClickMe)
-        button.setOnClickListener({
-            val intent = Intent(this, ResultsActivity::class.java)
-            startActivity(intent)
-        })
     }
-
-    //override fun onClick(v: View?) {
-    //    val intent = Intent(this, ResultsActivity::class.java)
-    //    startActivity(intent)
-    //}
 }
